851e7125e2d71857c6beb56f24400eb14cd00fe2
Simon Tan <simon.tan@and.digital>
2022-08-22 15:42:39 +0100

    initial firebase setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState}from 'react';
 import Hero from './components/hero';
 import Sidebar from './components/sidebar';
 import Content from './components/content';
 import Navbar from './components/navbar';
 
 function App() {
+
+  const [data, setData] = useState()
+
+  useEffect( async () => {
+    
+    const data = await readOpportunities()
+    setData(data)
+  }, []);
+
+
+
+
   return (
     <div
       className='grid grid-areas-smlayout grid-cols-smlayout grid-rows-smlayout 
