416186effda76f4f7561897fe76d09ce1cc2fc4b
Ross Middleton <5366498+R-Middleton@users.noreply.github.com>
2022-08-23 21:51:40 +0100

    refactor: :art: restyle hero section

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,10 +3,20 @@ import OpportunityLg from "./opportunityLg";
 
 const Hero = () => {
   return (
-    <div className="hidden grid-in-hero md:hero ">
+    <div className="bg-primary hidden grid-in-hero md:hero ">
       <div className="hero-content flex-col">
         <div className="flex flex-col gap-4 items-center md:flex-row md:gap-8">
-          <h1 className="text-5xl font-bold text-primary">Find AND Help</h1>
+          <h1 className="flex gap-2 font-roboto font-bold text-7xl text-white">
+            Find
+            <svg className="h-20 fill-white" viewBox="0 0 315.81 126.72">
+              <path d="M0 0h323.25v122.74H0z" />
+              <path
+                className="fill-primary"
+                d="M200.51 100.25h-18.83L141.57 53.2v47.05h-18.82V22.49h18.82l40.11 47v-47h18.83zM261.88 22.49h-38.89v18.83h9.92v40.11h-9.92v18.83h38.89c21.44 0 38.88-17.44 38.88-38.88 0-21.45-17.44-38.89-38.88-38.89zm0 58.93h-10.14V41.31h10.14c11.06 0 20.05 9 20.05 20.05.01 11.07-8.99 20.06-20.05 20.06zM61.38 22.49c-21.44 0-38.88 17.44-38.88 38.88v38.89h18.83v-9.92h40.11v9.92h18.83V61.37c-.01-21.44-17.45-38.88-38.89-38.88zM81.43 71.5H41.32V61.36c0-11.06 9-20.05 20.05-20.05 11.06 0 20.05 8.99 20.05 20.05l.01 10.14z"
+              />
+            </svg>
+            Help
+          </h1>
           <OpportunityLg />
         </div>
       </div>
--- src/components/opportunityLg.js
+++ src/components/opportunityLg.js
@@ -6,9 +6,21 @@ const OpportunityLg = () => {
     <div>
       <label
         htmlFor="opportunity-modal-lg"
-        className="btn btn-secondary w-40 modal-button text-s md:w-48 md:text-white shadow-lg"
+        className="btn flex gap-2 items-center btn-white text-black w-40 modal-button text-s md:w-48 shadow-lg hover:bg-slate-800 hover:text-white hover:border-slate-800"
       >
         Add Opportunity
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-6 h-6 fill-primary"
+        >
+          <path
+            fillRule="evenodd"
+            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z"
+            clipRule="evenodd"
+          />
+        </svg>
       </label>
       <input
         type="checkbox"
