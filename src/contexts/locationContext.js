de720e59afdef5a710a6afd3cd7c9af3a80a0277
Gavin Roderick <gavin@Y97CW52JFW.lan>
2022-08-23 15:37:30 +0100

    style: run prettier on project

--- src/contexts/locationContext.js
+++ src/contexts/locationContext.js
@@ -1,9 +1,9 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext } from "react";
 
 export const LocationContext = createContext();
 
 export const LocationContextProvider = ({ children }) => {
-  const locations = ['Glasgow', 'Edinburgh', 'London', 'Manchester'];
+  const locations = ["Glasgow", "Edinburgh", "London", "Manchester"];
   return (
     <LocationContext.Provider value={useState(locations)}>
       {children}
