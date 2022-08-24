008aef8e0d6a26e51675831f10e23e77959f9d8c
Sam-Prince <samsamp@hotmail.co.uk>
2022-08-24 12:27:48 +0100

    Set google cal using real opportunity data

--- src/gcal/gcal.js
+++ src/gcal/gcal.js
@@ -1,4 +1,10 @@
-export const gotoGcal = () => {
-    const url = "https://calendar.google.com/calendar/u/0/r/eventedit?text=Community+Cleanup&dates=20220828T090000ZGB/20220828T170000ZGB&details=Clean+up&location=Leeds"
-    window.open(url, '_blank', 'noopener,noreferrer');
+export const gotoGcal = (opportunity) => {
+    return function () {
+        const title = (opportunity.title).replace(/\s/g, '+')
+        const date = (opportunity.dateFrom).replace(/-/g, '')
+        const description = (opportunity.description).replace(/\s/g, '+')
+        const location = (opportunity.location).replace(/\s/g, '+')
+        const url = "https://calendar.google.com/calendar/u/0/r/eventedit?text="+title+"&dates="+date+"T090000ZGB/"+date+"T170000ZGB&details="+description+"&location="+location
+        window.open(url, '_blank', 'noopener,noreferrer');
+    }
 }
