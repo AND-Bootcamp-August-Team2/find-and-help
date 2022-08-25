a0dbdbb26c81b90cc3794fb2f4c5889b257f8f41
Ross Middleton <5366498+R-Middleton@users.noreply.github.com>
2022-08-25 10:33:03 +0100

    feat: :sparkles: displays message when no opportunities are being displayed

--- src/components/content.js
+++ src/components/content.js
@@ -74,17 +74,27 @@ const Content = () => {
     filterToDate,
   ]);
 
-  return (
-    <div className="grid-in-content mr-6 h-max min-h-full p-6 w-full md:w-auto md:rounded-tr-2xl">
-      <ul className="flex flex-col gap-6">
-        {filteredOpportunities.map((opportunity, i) => (
-          <li key={opportunity + i}>
-            <OpportunityCard opportunity={opportunity} />
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  if (filteredOpportunities.length === 0) {
+    return (
+      <div className="grid-in-content mr-6 h-max min-h-full p-6 w-full md:w-auto md:rounded-tr-2xl">
+        <h2 className="flex flex-col m-12  text font-roboto text-2xl text-slate-600 h-screen pt-20 md:pt-0">
+          No Opportunities To Display
+        </h2>
+      </div>
+    );
+  } else {
+    return (
+      <div className="grid-in-content mr-6 h-max min-h-full p-6 w-full md:w-auto md:rounded-tr-2xl">
+        <ul className="flex flex-col gap-6">
+          {filteredOpportunities.map((opportunity, i) => (
+            <li key={opportunity + i}>
+              <OpportunityCard opportunity={opportunity} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 };
 
 export default Content;
